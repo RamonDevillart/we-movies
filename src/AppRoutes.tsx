22958ac5ae0c0b4bd3d9e1170969f77857b27c1e
Ramon Devillart Mendes <89326111+RamonDevillart@users.noreply.github.com>
2024-08-22 09:57:12 -0300

    Update AppRoutes.tsx

--- src/AppRoutes.tsx
+++ src/AppRoutes.tsx
@@ -5,7 +5,7 @@ import Home from "./pages/Home/Home";
 import Checkout from "./pages/Checkout/Checkout";
 import PostPurchase from "./pages/PostPurchase/PostPurchase";
 import Header from "./components/Header/Header";
-import Movies from "./components/Movies/Movies";
+
 
 const AppRoutes: React.FC = () => {
   return (
@@ -13,8 +13,7 @@ const AppRoutes: React.FC = () => {
       <Header />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/checkout" element={<Checkout />} />
-        <Route path="/movies" element={<Movies />} />
+        <Route path="/checkout" element={<Checkout />} />       
         <Route path="/post-purchase" element={<PostPurchase />} />
         <Route path="*" element={<div>Página não encontrada!</div>} />
       </Routes>
